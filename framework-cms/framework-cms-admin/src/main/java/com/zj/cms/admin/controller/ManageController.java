577fdba1bc7287194a7d2675eebb9804b30c4ba1
luckybearbear <jerry.hui@pexetech.com>
2019-01-26 11:31:04 +0800

    new(framework-cms-admin) 架构搭建

---
+++ framework-cms/framework-cms-admin/src/main/java/com/zj/cms/admin/controller/ManageController.java
@@ -0,0 +1,12 @@
+package com.zj.cms.admin.controller;
+
+import com.zj.common.base.BaseController;
+
+/**
+ * describe:后台controller
+ *
+ * @author zj
+ * @date 2019/01/26
+ */
+public class ManageController extends BaseController {
+}
